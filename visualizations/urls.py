69288e78feb7882f487b15c72dcea0119504d9f9
JoPuskar <puskarjoshi22@gmail.com>
2018-07-01 23:18:33 +0545

    Charts Done

--- visualizations/urls.py
+++ visualizations/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'visualizations'
+
 urlpatterns = [
     path('', views.Dashboard.as_view(), name='dashboard'),
     path('housing-create', views.HousingCompletionCreate.as_view(), name="housing_create"),
     path('reconstruction-create', views.ReconstructionGrantCreate.as_view(), name="reconstruction_create"),
     path('recent-story-create', views.RecentStoryCreate.as_view(), name="recent_story_create"),
+    path('housing-update/<int:pk>', views.HousingCompletionUpdate.as_view(), name="housing_update"),
+    path('reconstruction-update/<int:pk>', views.RecentStoryUpdate.as_view(), name="reconstruction-update"),
+    path('recent-story-update/<int:pk>', views.RecentStoryUpdate.as_view(), name="recent_story_update"),
 ]
--- visualizations/views.py
+++ visualizations/views.py
@@ -1,24 +1,59 @@
-from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, UpdateView, ListView
 from .models import HousingCompletion, ReconstructionGrant, RecentStory
+
 
 
 class Dashboard(TemplateView):
     template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['housing_label'] = list(HousingCompletion.objects.values_list('label', flat=True))
+        context['reconstruction_label'] = list(ReconstructionGrant.objects.values_list('label', flat=True))
+        context['housing_values'] = list(HousingCompletion.objects.values_list('value', flat=True))
+        context['reconstruction_values'] = list(ReconstructionGrant.objects.values_list('value', flat=True))
+        context['recent_story'] = RecentStory.objects.all()
+        return context
 
 
 class HousingCompletionCreate(CreateView):
     model = HousingCompletion
     template_name = "visualizations/housing_completion_create.html"
     fields = '__all__'
+    success_url = reverse_lazy("visualizations:dashboard")
+
+
+class HousingCompletionUpdate(UpdateView):
+    model = HousingCompletion
+    template_name = "visualizations/housing_completion_create.html"
+    fields = '__all__'
+    success_url = reverse_lazy("visualizations:dashboard")
 
 
 class ReconstructionGrantCreate(CreateView):
     model = ReconstructionGrant
     template_name = "visualizations/reconstruction_grant_create.html"
     fields = '__all__'
+    success_url = reverse_lazy("visualizations:dashboard")
+
+
+class ReconstructionGrantUpdate(UpdateView):
+    model = HousingCompletion
+    template_name = "visualizations/housing_completion_create.html"
+    fields = '__all__'
+    success_url = reverse_lazy("visualizations:dashboard")
 
 
 class RecentStoryCreate(CreateView):
     model = RecentStory
     template_name = "visualizations/recent_story_create.html"
     fields = '__all__'
+    success_url = reverse_lazy("visualizations:dashboard")
+
+
+class RecentStoryUpdate(UpdateView):
+    model = RecentStory
+    template_name = "visualizations/recent_story_create.html"
+    fields = '__all__'
+    success_url = reverse_lazy("visualizations:dashboard")
