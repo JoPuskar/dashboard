d0865b3aab7372899af34c20ff4a7eff066798d3
JoPuskar <puskarjoshi22@gmail.com>
2018-07-03 22:11:23 +0545

    dynamic table

--- visualizations/views.py
+++ visualizations/views.py
@@ -1,6 +1,8 @@
+from django.db.models import Sum
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, CreateView, UpdateView, ListView, DetailView
-from .models import HousingCompletion, ReconstructionGrant, RecentStory
+from django.views.generic import TemplateView, CreateView, UpdateView, DetailView
+from .models import HousingCompletion, ReconstructionGrant, RecentStory,\
+    District, Gaunpalika, Data
 
 
 class Dashboard(TemplateView):
@@ -9,13 +11,44 @@ class Dashboard(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['housing_label'] = list(HousingCompletion.objects.values_list('label', flat=True))
-        context['reconstruction_label'] = list(ReconstructionGrant.objects.values_list('label', flat=True))
         context['housing_values'] = list(HousingCompletion.objects.values_list('value', flat=True))
+
+        context['reconstruction_label'] = list(ReconstructionGrant.objects.values_list('label', flat=True))
         context['reconstruction_values'] = list(ReconstructionGrant.objects.values_list('value', flat=True))
+
         context['recent_story'] = RecentStory.objects.all()
+
         context['district_json_path'] = "/static/json/District.json"
         context['gorkha_json_path'] = "/static/json/Gorkha.json"
         context['nuwakot_json_path'] = "/static/json/Nuwakot.json"
+
+        context['gorkha_total_houses_stage_i'] = Data.objects.filter(gaunpalika__district__id=1).\
+                                                aggregate(hs1=Sum('houses_in_stage_i'))
+        context['gorkha_total_houses_stage_ii'] = Data.objects.filter(gaunpalika__district__id=1). \
+                                                 aggregate(hs2=Sum('houses_in_stage_ii'))
+        context['gorkha_total_houses_stage_iii'] = Data.objects.filter(gaunpalika__district__id=1). \
+                                                    aggregate(hs3=Sum('houses_in_stage_iii'))
+
+        context['gorkha_total_received_tranche_i'] = Data.objects.filter(gaunpalika__district__id=1).\
+                                        aggregate(rt1=Sum('received_tranche_i'))
+        context['gorkha_total_received_tranche_ii'] = Data.objects.filter(gaunpalika__district__id=1). \
+                                        aggregate(rt2=Sum('received_tranche_ii'))
+        context['gorkha_total_received_tranche_iii'] = Data.objects.filter(gaunpalika__district__id=1). \
+                                        aggregate(rt3=Sum('received_tranche_iii'))
+
+        context['nuwakot_total_houses_stage_i'] = Data.objects.filter(gaunpalika__district__id=2).\
+                                        aggregate(hs1=Sum('houses_in_stage_i'))
+        context['nuwakot_total_houses_stage_ii'] = Data.objects.filter(gaunpalika__district__id=2). \
+                                        aggregate(hs2=Sum('houses_in_stage_ii'))
+        context['nuwakot_total_houses_stage_iii'] = Data.objects.filter(gaunpalika__district__id=2). \
+                                        aggregate(hs3=Sum('houses_in_stage_iii'))
+
+        context['nuwakot_total_received_tranche_i'] = Data.objects.filter(gaunpalika__district__id=2).\
+                                        aggregate(rt1=Sum('received_tranche_i'))
+        context['nuwakot_total_received_tranche_ii'] = Data.objects.filter(gaunpalika__district__id=2). \
+                                        aggregate(rt2=Sum('received_tranche_ii'))
+        context['nuwakot_total_received_tranche_iii'] = Data.objects.filter(gaunpalika__district__id=2). \
+                                        aggregate(rt3=Sum('received_tranche_iii'))
         return context
 
 
