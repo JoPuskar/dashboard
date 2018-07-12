e31bbf21a833b36f5c5d4bfbc051c634fba1126d
JoPuskar <puskarjoshi22@gmail.com>
2018-07-12 16:23:36 +0545

    code updated

--- visualizations/views.py
+++ visualizations/views.py
@@ -22,57 +22,105 @@ class Dashboard(TemplateView):
         context['gorkha_json_path'] = "/static/json/Gorkha.json"
         context['nuwakot_json_path'] = "/static/json/Nuwakot.json"
 
-        context['total_houses_completed'] = Data.objects.aggregate(total_hc=Sum('houses_completed'))
+        all_data = {}
+        data = Data.objects.all()
 
-        context['total_houses_stage_i'] = Data.objects.aggregate(total_hs1=Sum('houses_in_stage_i'))
-        context['total_houses_stage_ii'] = Data.objects.aggregate(total_hs2=Sum('houses_in_stage_ii'))
-        context['total_houses_stage_iii'] = Data.objects.aggregate(total_hs3=Sum('houses_in_stage_iii'))
+        total_houses_completed = 0
+        total_houses_stage_i = 0
+        total_houses_stage_ii = 0
+        total_houses_stage_iii = 0
+        total_received_tranche_i = 0
+        total_received_tranche_ii = 0
+        total_received_tranche_iii = 0
+
+        sum_women_percentage = 0
+
+        gorkha_houses_completed = 0
+        gorkha_total_houses_stage_i = 0
+        gorkha_total_houses_stage_ii = 0
+        gorkha_total_houses_stage_iii = 0
+        gorkha_total_received_tranche_i = 0
+        gorkha_total_received_tranche_ii = 0
+        gorkha_total_received_tranche_iii = 0
+
+        nuwakot_houses_completed = 0
+        nuwakot_total_houses_stage_i = 0
+        nuwakot_total_houses_stage_ii = 0
+        nuwakot_total_houses_stage_iii = 0
+        nuwakot_total_received_tranche_i = 0
+        nuwakot_total_received_tranche_ii = 0
+        nuwakot_total_received_tranche_iii = 0
+
+        for data in data:
+            total_houses_completed += data.houses_completed
+            total_houses_stage_i += data.houses_in_stage_i
+            total_houses_stage_ii += data.houses_in_stage_ii
+            total_houses_stage_iii += data.houses_in_stage_iii
+
+            total_received_tranche_i += data.received_tranche_i
+            total_received_tranche_ii += data.received_tranche_ii
+            total_received_tranche_iii += data.received_tranche_iii
+
+            sum_women_percentage += data.women_percentage
+
+            if data.gaunpalika.district.name == 'Gorkha':
+                gorkha_houses_completed += data.houses_completed
+                gorkha_total_houses_stage_i += data.houses_in_stage_i
+                gorkha_total_houses_stage_ii += data.houses_in_stage_ii
+                gorkha_total_houses_stage_iii += data.houses_in_stage_iii
+
+                gorkha_total_received_tranche_i += data.received_tranche_i
+                gorkha_total_received_tranche_ii += data.received_tranche_ii
+                gorkha_total_received_tranche_iii += data.received_tranche_iii
+
+            if data.gaunpalika.district.name == 'Nuwakot':
+                nuwakot_houses_completed += data.houses_completed
+                nuwakot_total_houses_stage_i += data.houses_in_stage_i
+                nuwakot_total_houses_stage_ii += data.houses_in_stage_ii
+                nuwakot_total_houses_stage_iii += data.houses_in_stage_iii
+
+                nuwakot_total_received_tranche_i += data.received_tranche_i
+                nuwakot_total_received_tranche_ii += data.received_tranche_ii
+                nuwakot_total_received_tranche_iii += data.received_tranche_iii
+
+        all_data['total_houses_completed'] = total_houses_completed
+        all_data['total_houses_stage_i'] = total_houses_stage_i
+        all_data['total_houses_stage_ii'] = total_houses_stage_ii
+        all_data['total_houses_stage_iii'] = total_houses_stage_iii
+
+        all_data['total_received_tranche_i'] = total_received_tranche_i
+        all_data['total_received_tranche_ii'] = total_received_tranche_ii
+        all_data['total_received_tranche_iii'] = total_received_tranche_iii
+
+        all_data['gorkha_total_houses_completed'] = gorkha_houses_completed
+        all_data['gorkha_total_houses_stage_i'] = gorkha_total_houses_stage_i
+        all_data['gorkha_total_houses_stage_ii'] = gorkha_total_houses_stage_ii
+        all_data['gorkha_total_houses_stage_iii'] = gorkha_total_houses_stage_iii
+
+        all_data['gorkha_total_received_tranche_i'] = gorkha_total_received_tranche_i
+        all_data['gorkha_total_received_tranche_ii'] = gorkha_total_received_tranche_ii
+        all_data['gorkha_total_received_tranche_iii'] = gorkha_total_received_tranche_iii
+
+        all_data['nuwakot_total_houses_completed'] = nuwakot_houses_completed
+        all_data['nuwakot_total_houses_stage_i'] = nuwakot_total_houses_stage_i
+        all_data['nuwakot_total_houses_stage_ii'] = nuwakot_total_houses_stage_ii
+        all_data['nuwakot_total_houses_stage_iii'] = nuwakot_total_houses_stage_iii
+
+        all_data['nuwakot_total_received_tranche_i'] = nuwakot_total_received_tranche_i
+        all_data['nuwakot_total_received_tranche_ii'] = nuwakot_total_received_tranche_ii
+        all_data['nuwakot_total_received_tranche_iii'] = nuwakot_total_received_tranche_iii
+
+        context = {'all_data': all_data}
 
         sum_women_percentage = Data.objects.aggregate(sum_wp=Sum('women_percentage'))
         denom = Data.objects.aggregate(total=Count('women_percentage'))
         denom = denom['total']*100
         context['total_women_percentage'] = round((sum_women_percentage['sum_wp']/denom)*100)
 
-        context['total_received_tranche_i'] = Data.objects.aggregate(total_rt1=Sum('received_tranche_i'))
-        context['total_received_tranche_ii'] = Data.objects.aggregate(total_rt2=Sum('received_tranche_ii'))
-        context['total_received_tranche_iii'] = Data.objects.aggregate(total_rt3=Sum('received_tranche_iii'))
-
-        context['gorkha_houses_completed'] = Data.objects.filter(gaunpalika__district__id=1).aggregate(hcg=Sum('houses_completed'))
-
-        context['gorkha_total_houses_stage_i'] = Data.objects.filter(gaunpalika__district__id=1).\
-                                                aggregate(hs1=Sum('houses_in_stage_i'))
-        context['gorkha_total_houses_stage_ii'] = Data.objects.filter(gaunpalika__district__id=1). \
-                                                 aggregate(hs2=Sum('houses_in_stage_ii'))
-        context['gorkha_total_houses_stage_iii'] = Data.objects.filter(gaunpalika__district__id=1). \
-                                                    aggregate(hs3=Sum('houses_in_stage_iii'))
-
-        context['gorkha_total_received_tranche_i'] = Data.objects.filter(gaunpalika__district__id=1).\
-                                        aggregate(rt1=Sum('received_tranche_i'))
-        context['gorkha_total_received_tranche_ii'] = Data.objects.filter(gaunpalika__district__id=1). \
-                                        aggregate(rt2=Sum('received_tranche_ii'))
-        context['gorkha_total_received_tranche_iii'] = Data.objects.filter(gaunpalika__district__id=1). \
-                                        aggregate(rt3=Sum('received_tranche_iii'))
-
         sum_women_percentage = Data.objects.filter(gaunpalika__district__id=1).aggregate(sum_wp=Sum('women_percentage'))
         denom = Data.objects.filter(gaunpalika__district__id=1).aggregate(total=Count('women_percentage'))
         denom = denom['total']*100
         context['gorkha_women_percentage'] = round((sum_women_percentage['sum_wp']/denom)*100)
-
-        context['nuwakot_houses_completed'] = Data.objects.filter(gaunpalika__district__id=2).aggregate(hcn=Sum('houses_completed'))
-
-        context['nuwakot_total_houses_stage_i'] = Data.objects.filter(gaunpalika__district__id=2).\
-                                        aggregate(hs1=Sum('houses_in_stage_i'))
-        context['nuwakot_total_houses_stage_ii'] = Data.objects.filter(gaunpalika__district__id=2). \
-                                        aggregate(hs2=Sum('houses_in_stage_ii'))
-        context['nuwakot_total_houses_stage_iii'] = Data.objects.filter(gaunpalika__district__id=2). \
-                                        aggregate(hs3=Sum('houses_in_stage_iii'))
-
-        context['nuwakot_total_received_tranche_i'] = Data.objects.filter(gaunpalika__district__id=2).\
-                                        aggregate(rt1=Sum('received_tranche_i'))
-        context['nuwakot_total_received_tranche_ii'] = Data.objects.filter(gaunpalika__district__id=2). \
-                                        aggregate(rt2=Sum('received_tranche_ii'))
-        context['nuwakot_total_received_tranche_iii'] = Data.objects.filter(gaunpalika__district__id=2). \
-                                        aggregate(rt3=Sum('received_tranche_iii'))
 
         sum_women_percentage = Data.objects.filter(gaunpalika__district__id=2).aggregate(sum_wp=Sum('women_percentage'))
         denom = Data.objects.filter(gaunpalika__district__id=2).aggregate(total=Count('women_percentage'))
@@ -83,10 +131,13 @@ class Dashboard(TemplateView):
                             'houses_completed', 'houses_in_stage_i',\
                             'houses_in_stage_ii', 'houses_in_stage_iii', 'received_tranche_i',\
                             'received_tranche_ii', 'received_tranche_iii', 'women_percentage')
+
         context['data_values_nuwakot'] = Data.objects.filter(gaunpalika__district__id=2).values_list('gaunpalika__name','houses_completed',\
                             'houses_in_stage_i', 'houses_in_stage_ii', 'houses_in_stage_iii', 'received_tranche_i',\
                             'received_tranche_ii', 'received_tranche_iii', 'women_percentage')
+
         context['gorkha_data'] = Data.objects.filter(gaunpalika__district__id=1).values_list('gaunpalika__name')
+
         context['nuwa_data'] = Data.objects.filter(gaunpalika__district__id=2).values_list('gaunpalika__name')
         return context
 
